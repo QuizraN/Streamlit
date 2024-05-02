a9b8261b9d8e25c72671218041f9b1335b4cce88
Quizra <quizra.nousheen@sojern.com>
2024-05-02 17:31:31 +0530

    test the flow

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
+
+st.set_page_config(layout='wide')
 
 if __name__ == "__main__":
     st.title("Stramlit Demo Application")
-    st.snow()
+    # data_point =st.slider('Select a value', min_value=0, max_value=10)
+    # st.write("The Selcted value is:",data_point)
+    components.iframe("https://docs.google.com/presentation/d/1fkKdCXsdglEok_gjy7-eJa1i2WDUgMSQZdtNA1YcJlc/edit?usp=sharing", width=1700, height=1000)
+    
+    
